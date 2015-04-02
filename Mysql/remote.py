f2b3fcf887a21b80df52069b1ccf9f50e604a68f
Chris Johnson <afckazoo@gmail.com>
2015-04-02 00:14:17 -0700

    Funtional Mysql Remote User Script

--- Mysql/remote.py
+++ Mysql/remote.py
@@ -1,3 +1,4 @@
 name = input("Enter UserName ")
+database = input("Enter Database ")
 
-print("CREATE USER '" + name + "'@'%';")
+print("CREATE USER '" + name + "'@'%';\n GRANT USAGE ON *.* TO '" + name + "'@'%';\n GRANT ALL PRIVILEGES ON `" + database + "`.* TO '" + name + "'@'%' WITH GRANT OPTION; ")
