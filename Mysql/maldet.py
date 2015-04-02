4c905e9c40cccea9b7405f435ef72059493344e1
Chris Johnson <afckazoo@gmail.com>
2015-04-02 16:18:43 -0700

    Added Maldet Script, improved remote

---
+++ Mysql/maldet.py
@@ -0,0 +1,4 @@
+name = input("Enter Username ")
+folder = input("Enter Directory ")
+
+print("Maldet -a ~"+name+"/webapps/"+folder+" ")    
--- Mysql/remote.py
+++ Mysql/remote.py
@@ -1,4 +1,4 @@
 name = input("Enter UserName ")
 database = input("Enter Database ")
 
-print("CREATE USER '" + name + "'@'%';\n GRANT USAGE ON *.* TO '" + name + "'@'%';\n GRANT ALL PRIVILEGES ON `" + database + "`.* TO '" + name + "'@'%' WITH GRANT OPTION; ")
+print("CREATE USER '" + name + "'@'%';\n GRANT USAGE ON *.* TO '" + name + "'@'%';\n GRANT ALL PRIVILEGES ON `" + database + "`.* TO '" + name + "'@'%' WITH GRANT OPTION;\n SHOW GRANTS FOR '" + name + "'@'%';")
